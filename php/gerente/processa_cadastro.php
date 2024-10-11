9f460941be492b9ff0609fb7436a0e54b6b99e75
LZanata <leonardozana19@gmail.com>
2024-10-11 16:36:31 -0300

    commit

---
+++ php/gerente/processa_cadastro.php
@@ -0,0 +1,20 @@
+<?php
+include 'conexao.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $cpf = $_POST['cpf'];
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT); // Criptografando a senha
+    $cargo = $_POST['cargo'];
+    $data_contrat = 
+
+    // Inserir os dados no banco de dados
+    $query = "INSERT INTO funcionario (cpf, nome, email, senha, cargo, data_contrat, genero) VALUES ('$nome', '$email', '$senha')";
+    if (mysqli_query($conexao, $query)) {
+        echo "Usuário cadastrado com sucesso!";
+    } else {
+        echo "Erro ao cadastrar o usuário: " . mysqli_error($conexao);
+    }
+}
+?>
