6d0b23f63f6827e66912e0ffa87ddd60a216b4e8
LZanata <leonardozana19@gmail.com>
2024-10-11 17:40:58 -0300

    Update processa_cadastro.php

--- php/gerente/processa_cadastro.php
+++ php/gerente/processa_cadastro.php
@@ -5,7 +5,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $cpf = $_POST['cpf'];
     $nome = $_POST['nome'];
     $email = $_POST['email'];
-    $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT); // Criptografando a senha
+    $senha = $_POST['senha'];
+    $confirma_senha = $_POST['confirma_senha'];
     $cargo = $_POST['cargo'];
     $data_contrat = $_POST['data_contrat'];
     $genero = $_POST['genero'];
@@ -18,9 +19,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     // Criptografa a senha
     $senha_hashed = password_hash($senha, PASSWORD_DEFAULT);
-    
+
     // Inserir os dados no banco de dados
     $query = "INSERT INTO funcionario (cpf, nome, email, senha, cargo, data_contrat, genero) VALUES ('$nome', '$email', '$senha', '$cargo', '$data_contrat', '$genero')";
+    
     if (mysqli_query($conexao, $query)) {
         echo "Usuário cadastrado com sucesso!";
     } else {
