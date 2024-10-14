84a8766ca94a3829063d20cb74e411248751ffce
LZanata <leonardozana19@gmail.com>
2024-10-14 18:50:19 -0300

    commit

---
+++ js/cadastro_login/enviar_email.js
@@ -0,0 +1,23 @@
+function sendEmail() {
+    const templateParams = {
+        nomeAcademia: document.getElementById("nomeAcademia").value,
+        nomeGerente: document.getElementById("nomeGerente").value,
+        telefoneAcademia: document.getElementById("telefoneAcademia").value,
+        telefoneGerente: document.getElementById("telefoneGerente").value,
+        email: document.getElementById("email").value,
+        cep: document.getElementById("cep").value,
+        estado: document.getElementById("estado").value,
+        cidade: document.getElementById("cidade").value,
+        bairro: document.getElementById("bairro").value,
+        rua: document.getElementById("rua").value,
+        numero: document.getElementById("numero").value,
+        complemento: document.getElementById("complemento").value       
+    };
+
+    emailjs.send('service_zzwirtl', 'seu_template_id', templateParams)
+        .then(function(response) {
+            alert('E-mail enviado com sucesso!');
+        }, function(error) {
+            alert('Falha ao enviar e-mail: ' + error.text);
+        });
+}
