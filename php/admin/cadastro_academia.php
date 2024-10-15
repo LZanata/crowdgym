144ba31be62b06f793f73c03cb8f83a73369c962
LZanata <leonardozana19@gmail.com>
2024-10-15 17:50:29 -0300

    commit

--- php/admin/cadastro_academia.php
+++ php/admin/cadastro_academia.php
@@ -11,9 +11,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $cidade = $_POST['cidade'];
     $estado = $_POST['estado'];
     $cep = $_POST['cep'];
+    $dia_semana = $_POST['dia_semana'];
+    $abertura = $_POST['abertura'];
+    $fechamento = $_POST['fechamento'];
 
     // Inserir os dados no banco de dados
-    $query = "INSERT INTO academia (nome, telefone, rua, numero, complemento, bairro, cidade, estado, cep) VALUES ('$nome','$telefone', '$rua', '$numero', '$complemento', '$bairro', '$cidade', '$estado', '$cep')";
+    $query = "INSERT INTO academia (nome, telefone, rua, numero, complemento, bairro, cidade, estado, cep, dia_semana, abertura, fechamento) VALUES ('$nome','$telefone', '$rua', '$numero', '$complemento', '$bairro', '$cidade', '$estado', '$cep', '$dia_semana', '$abertura', '$fechamento')";
 
     if (mysqli_query($conexao, $query)) {
         echo "Usuário cadastrado com sucesso!";
