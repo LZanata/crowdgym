32772443357f13874e447e28847af8d5e5486867
LZanata <leonardozana19@gmail.com>
2024-10-16 19:07:08 -0300

    Update conexao.php

--- php/cadastro_login/conexao.php
+++ php/cadastro_login/conexao.php
@@ -1,14 +1,16 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "crowdgym";
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "crowdgym";
 
-// Cria a conexão
-$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
-
-// Verifica a conexão
-if (!$conexao) {
-    die("Falha na conexão: " . mysqli_connect_error());
+try {
+    // Cria a conexão com o banco de dados usando PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Configura o modo de erro do PDO para lançar exceções
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Em caso de erro, exibi a mensagem de erro
+    die("Erro ao conectar ao banco de dados: " . $e->getMessage());
 }
 ?>
