1c34c286cc0bd15aa0fd83ce1d373d920d5a2351
LZanata <leonardozana19@gmail.com>
2024-10-18 06:43:46 -0300

    Create cadastro_aluno.php

---
+++ php/aluno/cadastro_aluno.php
@@ -0,0 +1,42 @@
+<?php
+include 'conexao.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $cpf = $_POST['cpf'];
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $confirma_senha = $_POST['confirma_senha'];
+    $data_contrat = $_POST['data_nascimento'];
+    $genero = $_POST['genero'];
+
+    // Verifica se as senhas coincidem
+    if ($senha !== $confirma_senha) {
+        echo "Erro: As senhas não coincidem. Tente novamente.";
+        exit; // Interrompe a execução se as senhas não coincidirem
+    }
+
+    // Hash da senha
+    $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
+
+    // Inserir os dados no banco de dados
+    $query = "INSERT INTO aluno (cpf, nome, email, senha, genero, data_nascimento) VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = mysqli_prepare($conexao, $query);
+    mysqli_stmt_bind_param($stmt, 'sssssi', $cpf, $nome, $email, $senha_hash, $genero, $data_nascimento);
+
+    if (mysqli_stmt_execute($stmt)) {
+        echo "Usuário cadastrado com sucesso!";
+        // Redirecionar para outra página
+        header("Location: http://localhost/Projeto_CrowdGym/login_aluno.html");
+        exit();
+    } else {
+        echo "Erro ao cadastrar o usuário: " . mysqli_error($conexao);
+    }
+
+    // Fecha o statement
+    mysqli_stmt_close($stmt);
+}
+
+// Fecha a conexão
+mysqli_close($conexao);
+?>
