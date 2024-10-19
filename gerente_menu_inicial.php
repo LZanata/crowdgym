536f5203cc8f06a4c6b4c33af9f5c3ac276c3299
LZanata <leonardozana19@gmail.com>
2024-10-19 09:12:44 -0300

    commit

--- gerente_menu_inicial.php
+++ gerente_menu_inicial.php
@@ -1,9 +1,5 @@
-<? session_start();
-if (!isset($_SESSION['gerente_id'])) {
-    // Se o usuário não estiver logado, redireciona para o login
-    header("Location: processar_login_academia.php");
-    exit;
-}
+<? 
+ require 'verifica_login_gerente.php';
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
---
+++ php/cadastro_login/verifica_login_gerente.php
@@ -0,0 +1,7 @@
+<? session_start();
+if (!isset($_SESSION['gerente_id'])) {
+    // Se o usuário não estiver logado, redireciona para o login
+    header("Location: processar_login_academia.php");
+    exit;
+}
+?>
