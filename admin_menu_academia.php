a5d3f3ebcb726e297caceac61a8f57f54ccf9873
LZanata <leonardozana19@gmail.com>
2024-10-21 17:25:19 -0300

    alterando css

--- admin_menu_academia.php
+++ admin_menu_academia.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Administrador Menu</title>
+    <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/admin/menu_academia.css" />
     <link
       rel="stylesheet"
--- admin_menu_gerente.php
+++ admin_menu_gerente.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Administrador Menu Gerente</title>
+    <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/admin/menu_gerente.css" />
     <link
       rel="stylesheet"
