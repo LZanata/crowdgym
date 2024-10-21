e52fd9186a32537266b20c320cf9f6cf183a1cce
LZanata <leonardozana19@gmail.com>
2024-10-21 17:21:09 -0300

    alterando css

--- gerente_func.php
+++ gerente_func.php
@@ -189,6 +189,12 @@
     </div>
     </div>
   </main>
+  <footer>
+    <div id="footer_copyright">
+      &#169
+      2024 CROWD GYM FROM EASY SYSTEM LTDA
+    </div>
+  </footer>
 </body>
 
 </html>
