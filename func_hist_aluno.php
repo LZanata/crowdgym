3e56412112858351f69d689a8a9aaf3351e24135
LZanata <leonardozana19@gmail.com>
2024-10-27 08:03:56 -0300

    alterando parte dos funcionarios

--- func_hist_aluno.php
+++ func_hist_aluno.php
@@ -1,59 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Histórico Aluno</title>
-    <link rel="stylesheet" href="css/index.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Histórico Aluno</title>
+  <link rel="stylesheet" href="css/index.css">
 </head>
+
 <body>
-    <!--Aqui o funcionário poderá ver o histórico dos alunos que entraram e sairam da academia-->
-    <header>
-        <nav>
-          <!--Menu para alterar as opções de tela-->
-          <div class="list">
-            <ul>
-              <li class="dropdown">
-                <a href="#"><i class="bi bi-list"></i></a>
-  
-                <div class="dropdown-list">
-                  <a href="aluno_menu_inicial.html">Menu Inicial</a>
-                  <a href="aluno_minhas_academias.html">Minhas Academias</a>
-                  <a href="aluno_buscar_academias.html">Buscar Academias</a>
-                  <a href="aluno_dados_pagamento.html">Dados de Pagamento</a>
-                  <a href="aluno_sobre_nos.html">Sobre Nós</a>
-                  <a href="aluno_suporte.html">Ajuda e Suporte</a>
-                  <a href="tela_inicio.html">Sair</a>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <!--Logo do Crowd Gym(quando passar o mouse por cima, o logo devera ficar laranja)-->
-          <div class="logo">
-            <h1>Crowd Gym</h1>
-          </div>
-          <!--Opção para alterar as configurações de usuário-->
-          <div class="user">
-            <ul>
-              <li class="user-icon">
-                <a href=""><i class="bi bi-person-circle"></i></a>
-  
-                <div class="dropdown-icon">
-                  <a href="#">Editar Perfil</a>
-                  <a href="#">Alterar Tema</a>
-                  <a href="#">Sair da Conta</a>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </header>
-      <main></main>
-    <footer>
-        <div id="footer_copyright">
-          &#169
-          2024 CROWD GYM FROM EASY SYSTEM LTDA
-        </div>
-      </footer>
+  <!--Aqui o funcionário poderá ver o histórico dos alunos que entraram e sairam da academia-->
+  <header>
+    <nav>
+      <!--Menu para alterar as opções de tela-->
+      <div class="list">
+        <ul>
+          <li class="dropdown">
+            <a href="#"><i class="bi bi-list"></i></a>
+
+            <div class="dropdown-list">
+              <a href="aluno_menu_inicial.php">Menu Inicial</a>
+              <a href="aluno_minhas_academias.php">Minhas Academias</a>
+              <a href="aluno_buscar_academias.php">Buscar Academias</a>
+              <a href="aluno_dados_pagamento.php">Dados de Pagamento</a>
+              <a href="aluno_sobre_nos.php">Sobre Nós</a>
+              <a href="aluno_suporte.php">Ajuda e Suporte</a>
+              <a href="tela_inicio.html">Sair</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <!--Logo do Crowd Gym(quando passar o mouse por cima, o logo devera ficar laranja)-->
+      <div class="logo">
+        <h1>Crowd Gym</h1>
+      </div>
+      <!--Opção para alterar as configurações de usuário-->
+      <div class="user">
+        <ul>
+          <li class="user-icon">
+            <a href=""><i class="bi bi-person-circle"></i></a>
+
+            <div class="dropdown-icon">
+              <a href="#">Editar Perfil</a>
+              <a href="#">Alterar Tema</a>
+              <a href="#">Sair da Conta</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <main></main>
+  <footer>
+    <div id="footer_copyright">
+      &#169
+      2024 CROWD GYM FROM EASY SYSTEM LTDA
+    </div>
+  </footer>
 </body>
+
 </html>
