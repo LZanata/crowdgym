dd6a049cf8a8b63a9850461f4ac9b4d7fffa0163
LZanata <leonardozana19@gmail.com>
2024-11-22 16:05:14 -0300

    configurando pix

---
+++ lib/crowdgym-pix/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'crowdgym/pix',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3b5bb8c4c7eb73a2dffb0a3ef5120588ab739c6f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'crowdgym/pix' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3b5bb8c4c7eb73a2dffb0a3ef5120588ab739c6f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
