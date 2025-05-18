c0af386fac6b2ce3198f651399450a05f2abd5b9
Guilherme <163676942+GuilhermePego@users.noreply.github.com>
2025-05-18 12:10:55 -0300

    Suporte
    Adicionei O icone de suporte em cada uma das telas dos alunos

--- aluno/configuracoes.php
+++ aluno/configuracoes.php
@@ -10,7 +10,19 @@ require_once '../php/cadastro_login/check_login_aluno.php';
     <title>Configurações Aluno</title>
     <link rel="stylesheet" href="../css/index.css">
 </head>
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6827a6cd36f29c190d216342/1irdh4qa7';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 <body>
     <!--Quando clicar aparecerá as opções para o usuario poder fazer alterar: conta(nome do aluno, email, autenticação de dois fatores e senha) - tema(claro ou escuro) -->
     <?php include '../partials/header_aluno.php'; ?> <!-- Inclui o cabeçalho -->
--- aluno/dados_pagamento.php
+++ aluno/dados_pagamento.php
@@ -14,7 +14,19 @@ require_once '../php/cadastro_login/check_login_aluno.php';
     rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
 </head>
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6827a6cd36f29c190d216342/1irdh4qa7';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 <body>
   <!--Quando clicar ir para área onde pode adcionar ou remover uma forma de pagamento -->
   <?php include '../partials/header_aluno.php'; ?> <!-- Inclui o cabeçalho -->
