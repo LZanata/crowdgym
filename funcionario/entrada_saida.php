5c8b66ef77db7f90fee928e96008c083fd0b4e13
Guilherme <163676942+GuilhermePego@users.noreply.github.com>
2025-05-18 16:32:29 -0300

    Suporte
    Adicionado o icone de suporte na aba dos funcionários.

--- funcionario/entrada_saida.php
+++ funcionario/entrada_saida.php
@@ -29,7 +29,19 @@ $resultado = $query->get_result();
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
 </head>
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6827a6cd36f29c190d216342/1irdh4qa7';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 <body>
     <!--Aqui o funcionário poderá ver o registro dos alunos que entraram e sairam da academia-->
     <?php include '../partials/header_funcionario.php'; ?> <!-- Inclui o cabeçalho -->
